207277d8f4eaa525aecc72c5366737d11bf3e1ab
alejandro <alejandroalfonso1994@gmail.com>
2024-07-13 23:38:13 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,191 @@
-# arikedb-rust
-Rust Library to connect to Arikedb
+
+# ArikeDB Rust Library
+
+Welcome to the ArikeDB Rust library! This library provides an interface to interact with the Arike Database, an advanced real-time database solution. This documentation will guide you through the process of setting up and using the library effectively.
+
+## Getting Started
+
+### Installation
+
+To use the ArikeDB Rust library, add it to your `Cargo.toml`:
+
+```toml
+[dependencies]
+arikedb = "*"
+```
+
+### Connecting to ArikeDB
+
+To connect to an ArikeDB server instance, bring `ArikedbClient` into scope and instantiate it by calling the `connect` function.
+
+#### Basic Connection
+
+```rust
+use arikedb::ArikedbClient;
+use arikedb::{Variable, VariableType, Epoch, VarEvent, Event};
+
+#[tokio::main]
+async fn main() {
+    let mut client = ArikedbClient::connect(
+        "127.0.0.1", // host
+        6923,        // port
+        false        // use_ssl
+    ).await.unwrap();
+}
+```
+
+#### Connection with Authentication
+
+If the server requires authentication, you need to authenticate after connecting.
+
+```rust
+use arikedb::ArikedbClient;
+use arikedb::{Variable, VariableType, Epoch, VarEvent, Event};
+
+#[tokio::main]
+async fn main() {
+    let mut client = ArikedbClient::connect(
+        "127.0.0.1", // host
+        6923,        // port
+        false        // use_ssl
+    ).await.unwrap();
+
+    client.authenticate("username", "password").await.unwrap();
+}
+```
+
+### Creating Collections
+
+ArikeDB organizes data into collections. Each collection has a name and a set of variables. To create multiple collections in a single call:
+
+```rust
+client.create_collections(&vec!["collection1", "collection2", "collection3"]).await.unwrap();
+```
+
+### Deleting Collections
+
+```rust
+client.delete_collections(&vec!["collection2", "collection3"]).await.unwrap();
+```
+
+### Listing Collections
+
+```rust
+let collections = client.list_collections().await.unwrap();
+
+for collection in collections {
+    println!("{:?}", collection);
+}
+```
+Output:
+```
+Collection { name: "collection1" }
+Collection { name: "collection2" }
+```
+
+### Creating Variables
+
+```rust
+let variables = vec![
+    Variable { name: String::from("var1"), vtype: VariableType::I32, buffer_size: 10 },
+    Variable { name: String::from("var2"), vtype: VariableType::I32, buffer_size: 5 },
+    Variable { name: String::from("var3"), vtype: VariableType::I32, buffer_size: 4 },
+    Variable { name: String::from("var4"), vtype: VariableType::I32, buffer_size: 4 },
+];
+
+client.create_variables("collection1", variables).await.unwrap();
+```
+
+### Deleting Variables
+
+```rust
+client.delete_variables("collection1", &vec!["var3", "var4"]).await.unwrap();
+```
+
+### Listing Variables
+
+```rust
+let variables = client.list_variables("collection1").await.unwrap();
+
+for variable in variables {
+    println!("{:?}", variable);
+}
+```
+Output:
+```
+Variable { name: "var1", vtype: I32, buffer_size: 10 }
+Variable { name: "var2", vtype: I32, buffer_size: 5 }
+```
+
+### Setting Variables Values
+
+```rust
+client.set_variables(
+    "collection1",
+    vec!["var1", "var2"],
+    SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_micros(),
+    vec!["-235", "48"],
+    Epoch::Microsecond
+).await.unwrap();
+```
+
+### Getting Variables Values
+
+```rust
+let derived_order = 0;
+let data = client.get_variables("collection1", vec!["var1", "var2"], derived_order, Epoch::Nanosecond).await.unwrap();
+
+for point in data {
+    println!("{:?}", point);
+}
+```
+Output:
+```
+DataPoint { name: "var1", vtype: I32, timestamp: "1720927547279880000", epoch: Nanosecond, value: "-235" }
+DataPoint { name: "var2", vtype: I32, timestamp: "1720927547279880000", epoch: Nanosecond, value: "48" }
+```
+
+### Subscribe to Variables Events
+Events are generated over variables when they ar set and event condition happens.
+
+```rust
+tokio::spawn(
+    client.subscribe_variables(
+        "collection1",
+        vec!["var1", "var2"],
+        vec![
+            VarEvent { event: Event::OnRise, ..Default::default() },
+            VarEvent { event: Event::OnValueEqVal, value: String::from("56"), ..Default::default() },
+        ],
+        |point| {
+            println!("{:?}", point);
+        }
+    ).await.unwrap()
+);
+
+for i in 0..10 {
+    client.set_variables(
+        "collection1",
+        vec!["var1", "var2"],
+        SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_micros(),
+        vec![(50 + i).to_string().as_str(), (60 - i).to_string().as_str()],
+        Epoch::Microsecond
+    ).await.unwrap();
+    tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+}
+```
+Output:
+```
+DataPoint { name: "var1", vtype: I32, timestamp: "1720927881619292000", epoch: Nanosecond, value: "50" }
+DataPoint { name: "var2", vtype: I32, timestamp: "1720927881619292000", epoch: Nanosecond, value: "60" }
+DataPoint { name: "var1", vtype: I32, timestamp: "1720927882629171000", epoch: Nanosecond, value: "51" }
+DataPoint { name: "var1", vtype: I32, timestamp: "1720927883638571000", epoch: Nanosecond, value: "52" }
+DataPoint { name: "var1", vtype: I32, timestamp: "1720927884652619000", epoch: Nanosecond, value: "53" }
+DataPoint { name: "var1", vtype: I32, timestamp: "1720927885664589000", epoch: Nanosecond, value: "54" }
+DataPoint { name: "var2", vtype: I32, timestamp: "1720927885664589000", epoch: Nanosecond, value: "56" }
+DataPoint { name: "var1", vtype: I32, timestamp: "1720927886673866000", epoch: Nanosecond, value: "55" }
+DataPoint { name: "var1", vtype: I32, timestamp: "1720927887693254000", epoch: Nanosecond, value: "56" }
+DataPoint { name: "var1", vtype: I32, timestamp: "1720927888703026000", epoch: Nanosecond, value: "57" }
+DataPoint { name: "var1", vtype: I32, timestamp: "1720927889717038000", epoch: Nanosecond, value: "58" }
+DataPoint { name: "var1", vtype: I32, timestamp: "1720927890728150000", epoch: Nanosecond, value: "59" }
+```
---
+++ src/lib.rs
@@ -0,0 +1,480 @@
+use tonic::{Request, Response};
+use tonic::transport::Channel;
+use arikedbpbuff::arikedb_rpc_client::ArikedbRpcClient;
+use arikedbpbuff::{
+    StatusCode,
+    CollectionMeta,
+    VariableMeta,
+    VarDataPoint,
+    VariableEvent,
+    ListCollectionsRequest,
+    CreateCollectionsRequest,
+    DeleteCollectionsRequest,
+    ListVariablesRequest,
+    CreateVariablesRequest,
+    DeleteVariablesRequest,
+    SetVariablesRequest,
+    GetVariablesRequest,
+    SubscribeVariablesRequest,
+    AuthenticateRequest,
+};
+
+mod arikedbpbuff { tonic::include_proto!("arikedbpbuff"); }
+
+
+#[derive(Debug)]
+pub enum Epoch {
+    Second,
+    Millisecond,
+    Microsecond,
+    Nanosecond
+}
+
+#[derive(Debug)]
+pub struct Variable {
+    pub name: String,
+    pub vtype: VariableType,
+    pub buffer_size: u32,
+}
+
+#[derive(Debug)]
+pub struct Collection {
+    pub name: String,
+}
+
+#[derive(Debug)]
+pub struct DataPoint {
+    pub name: String,
+    pub vtype: VariableType,
+    pub timestamp: String,
+    pub epoch: Epoch,
+    pub value: String,
+}
+
+
+#[derive(Debug, Default)]
+pub enum VariableType {
+    I8 = 0,
+    I16 = 1,
+    I32 = 2,
+    I64 = 3,
+    I128 = 4,
+    U8 = 5,
+    U16 = 6,
+    U32 = 7,
+    U64 = 8,
+    U128 = 9,
+    F32 = 10,
+    #[default]
+    F64 = 11,
+    STR = 12,
+    BOOL = 13,
+}
+
+#[derive(Debug, Default)]
+pub enum Event {
+    #[default]
+    OnSet = 0,
+    OnChange = 1,
+    OnRise = 2,
+    OnFall = 3,
+    OnValueReachVal = 4,
+    OnValueEqVal = 5,
+    OnValueLeaveVal = 6,
+    OnValueDiffVal = 7,
+    OnCrossHighLimit = 8,
+    OnCrossLowLimit = 9,
+    OnOverHighLimit = 10,
+    OnUnderLowLimit = 11,
+    OnValueReachRange = 12,
+    OnValueInRange = 13,
+    OnValueLeaveRange = 14,
+    OnValueOutRange = 15,
+}
+
+#[derive(Debug, Default)]
+pub struct VarEvent {
+    pub event: Event,
+    pub value: String,
+    pub low_limit: String,
+    pub high_limit: String,
+}
+
+pub struct ArikedbClient {
+    client: ArikedbRpcClient<Channel>,
+    pub token: Option<String>,
+}
+
+
+impl ArikedbClient {
+
+    pub async fn connect(
+        host: &str,
+        port: u16,
+        use_ssl: bool
+    ) -> Result<Self, Box<dyn std::error::Error>> {
+
+        let url = if use_ssl { format!("https://{}:{}", host, port) } else { format!("http://{}:{}", host, port) };
+
+        match Channel::from_shared(url) {
+            Ok(endpoint) => {
+                match endpoint.connect().await {
+                    Ok(channel) => {
+                        Ok(Self { client: ArikedbRpcClient::new(channel), token: None })
+                    },
+                    Err(err) => return Err(err.into()),
+                }
+            }
+            Err(err) => Err(err.into()),
+        }
+
+    }
+
+    pub async fn list_collections(
+        &mut self
+    ) -> Result<Vec<Collection>, Box<dyn std::error::Error>> {
+
+        let mut request = Request::new(ListCollectionsRequest {});
+        self._insert_meta(&mut request);
+
+        match self.client.list_collections(request).await {
+            Ok(response) => {
+                self._read_meta(&response);
+                Ok(ArikedbClient::_from_coll_meta(response.into_inner().collections))
+            }
+            Err(err) => Err(err.into()),
+        }
+
+    }
+
+    pub async fn create_collections(
+        &mut self,
+        name: &Vec<&str>
+    ) -> Result<(), Box<dyn std::error::Error>> {
+
+        let collections = name.iter().map(|n| CollectionMeta { name: n.to_string() }).collect();
+        let mut request = Request::new(CreateCollectionsRequest { collections });
+        self._insert_meta(&mut request);
+
+        match self.client.create_collections(request).await {
+            Ok(response) => {
+                self._read_meta(&response);
+                Ok(())
+            },
+            Err(err) => Err(err.into()),
+        }
+
+    }
+
+    pub async fn delete_collections(
+        &mut self,
+        names: &Vec<&str>
+    ) -> Result<(), Box<dyn std::error::Error>> {
+
+        let mut request = Request::new(DeleteCollectionsRequest { names: names.iter().map(|n| n.to_string()).collect() });
+        self._insert_meta(&mut request);
+
+        match self.client.delete_collections(request).await {
+            Ok(response) => {
+                self._read_meta(&response);
+                Ok(())
+            },
+            Err(err) => Err(err.into()),
+        }
+
+    }
+
+    pub async fn list_variables(
+        &mut self,
+        collection: &str
+    ) -> Result<Vec<Variable>, Box<dyn std::error::Error>> {
+
+        let mut request = Request::new(ListVariablesRequest { collection: collection.to_string() });
+        self._insert_meta(&mut request);
+
+        match self.client.list_variables(request).await {
+            Ok(response) => {
+                self._read_meta(&response);
+                Ok(ArikedbClient::_from_var_meta(response.into_inner().variables))
+            }
+            Err(err) => Err(err.into()),
+        }
+
+    }
+
+    pub async fn create_variables(
+        &mut self,
+        collection: &str,
+        variables: Vec<Variable>
+    ) -> Result<(), Box<dyn std::error::Error>> {
+
+        let vars_meta = variables.into_iter().map(|v| VariableMeta {
+            name: v.name,
+            vtype: v.vtype as i32,
+            buffer_size: v.buffer_size
+        }).collect();
+
+        let mut request = Request::new(CreateVariablesRequest { collection: collection.to_string(), variables: vars_meta });
+        self._insert_meta(&mut request);
+
+        match self.client.create_variables(request).await {
+            Ok(response) => {
+                self._read_meta(&response);
+                Ok(())
+            }
+            Err(err) => Err(err.into()),
+        }
+
+    }
+
+    pub async fn delete_variables(
+        &mut self,
+        collection: &str,
+        names: &Vec<&str>
+    ) -> Result<(), Box<dyn std::error::Error>> {
+
+        let mut request = Request::new(DeleteVariablesRequest { collection: collection.to_string(), names: names.iter().map(|n| n.to_string()).collect() });
+        self._insert_meta(&mut request);
+
+        match self.client.delete_variables(request).await {
+            Ok(response) => {
+                self._read_meta(&response);
+                Ok(())
+            }
+            Err(err) => Err(err.into()),
+        }
+
+    }
+
+    pub async fn set_variables(
+        &mut self,
+        collection: &str,
+        names: Vec<&str>,
+        timestamp: u128,
+        values: Vec<&str>,
+        epoch: Epoch
+    ) -> Result<(), Box<dyn std::error::Error>> {
+
+        let mut requset = Request::new(
+            SetVariablesRequest {
+                collection: collection.to_string(),
+                names: names.iter().map(|n| n.to_string()).collect(),
+                timestamp: timestamp.to_string(),
+                values: values.iter().map(|v| v.to_string()).collect(),
+                epoch: epoch as i32
+            }
+        );
+        self._insert_meta(&mut requset);
+
+        match self.client.set_variables(requset).await {
+            Ok(response) => {
+                self._read_meta(&response);
+                Ok(())
+            },
+            Err(err) => Err(err.into()),
+        }
+
+    }
+
+    pub async fn get_variables(
+        &mut self,
+        collection: &str,
+        names: Vec<&str>,
+        derived_order: u32,
+        epoch: Epoch
+    ) -> Result<Vec<DataPoint>, Box<dyn std::error::Error>> {
+
+        let mut request = Request::new(GetVariablesRequest { collection: collection.to_string(), names: names.iter().map(|n| n.to_string()).collect(), derived_order, epoch: epoch as i32 });
+        self._insert_meta(&mut request);
+
+        match self.client.get_variables(request).await {
+            Ok(response) => {
+                self._read_meta(&response);
+                Ok(ArikedbClient::_cast_data_points(response.into_inner().points))
+            }
+            Err(err) => Err(err.into()),
+        }
+
+    }
+
+    pub async fn subscribe_variables<F>(
+        &mut self,
+        collection: &str,
+        names: Vec<&str>,
+        events: Vec<VarEvent>,
+        callback: F
+    ) -> Result<tokio::task::JoinHandle<()>, Box<dyn std::error::Error>>
+    where
+        F: Fn(DataPoint) -> () + Send + 'static
+    {
+
+        let mut request = Request::new(SubscribeVariablesRequest { collection: collection.to_string(), names: names.iter().map(|n| n.to_string()).collect(), events: ArikedbClient::_cast_var_events(events) });
+        self._insert_meta(&mut request);
+
+        match self.client.subscribe_variables(request).await {
+            Ok(response) => {
+                self._read_meta(&response);
+                let mut stream = response.into_inner();
+
+                let handler = tokio::spawn(async move {
+                    while let Ok(Some(point)) = stream.message().await {
+                        callback(ArikedbClient::_cast_data_point(point));
+                    }
+                });
+
+                Ok(handler)
+            },
+            Err(err) => Err(err.into()),
+        }
+
+    }
+
+    pub async fn authenticate(
+        &mut self,
+        username: &str,
+        password: &str
+    ) -> Result<(), Box<dyn std::error::Error>> {
+
+        match self.client.authenticate(AuthenticateRequest {
+            username: username.to_string(),
+            password: password.to_string()
+        }).await {
+            Ok(response) => {
+                let resp = response.into_inner();
+                let status = StatusCode::try_from(resp.status);
+                match status {
+                    Ok(status_code) => {
+                        match status_code {
+                            StatusCode::Ok => {
+                                self.token = Some(resp.token);
+                                Ok(())
+                            },
+                            StatusCode::Unauthorized => Err("Unauthorized".into()),
+                            _ => Err("Unknown error".into()),
+                        }
+                    }
+                    Err(_) => Err("Unknown error".into()),
+                }
+            },
+            Err(err) => Err(err.into()),
+        }
+
+    }
+
+    fn _insert_meta<T>(&self, req: &mut Request<T>) {
+        match self.token.clone() {
+            Some(token) => {
+                req.metadata_mut().insert("authorization", token.parse().unwrap());
+            }
+            None => {},
+        };
+    }
+
+    fn _read_meta<T>(&mut self, response: &Response<T>) {
+
+        let metadata = response.metadata();
+        match metadata.get("refresh_token") {
+            Some(token) => {
+                self.token = Some(token.to_str().unwrap().to_string());
+            },
+            None => ()
+        };
+
+    }
+
+    fn _from_coll_meta(coll_meta: Vec<CollectionMeta>) -> Vec<Collection> {
+
+        coll_meta.into_iter().map(|meta| Collection { name: meta.name }).collect()
+
+    }
+
+    fn _from_var_meta(var_meta: Vec<VariableMeta>) -> Vec<Variable> {    
+
+        var_meta.into_iter().map(|meta| Variable {
+            name: meta.name,
+            vtype: ArikedbClient::_variabe_type_cast(meta.vtype),
+            buffer_size: meta.buffer_size
+        }).collect()
+
+    }
+
+    fn _variabe_type_cast(value: i32) -> VariableType {
+        match value {
+            0 => VariableType::I8,
+            1 => VariableType::I16,
+            2 => VariableType::I32,
+            3 => VariableType::I64,
+            4 => VariableType::I128,
+            5 => VariableType::U8,
+            6 => VariableType::U16,
+            7 => VariableType::U32,
+            8 => VariableType::U64,
+            9 => VariableType::U128,
+            10 => VariableType::F32,
+            11 => VariableType::F64,
+            12 => VariableType::STR,
+            13 => VariableType::BOOL,
+            _ => VariableType::I8
+        }
+    }
+
+    fn _epoch_cast(value: i32) -> Epoch {
+        match value {
+            0 => Epoch::Second,
+            1 => Epoch::Millisecond,
+            2 => Epoch::Microsecond,
+            3 => Epoch::Nanosecond,
+            _ => Epoch::Second
+        }
+    }
+
+    fn _cast_data_point(point: VarDataPoint) -> DataPoint {
+        DataPoint {
+            name: point.name,
+            vtype: ArikedbClient::_variabe_type_cast(point.vtype),
+            timestamp: point.timestamp,
+            epoch: ArikedbClient::_epoch_cast(point.epoch),
+            value: point.value
+        }
+    }
+
+    fn _cast_data_points(data_point: Vec<VarDataPoint>) -> Vec<DataPoint> {
+
+        data_point.into_iter().map(|point| ArikedbClient::_cast_data_point(point)).collect()
+
+    }
+
+    fn _event_cast(value: i32) -> Event {
+        match value {
+            0 => Event::OnSet,
+            1 => Event::OnChange,
+            2 => Event::OnRise,
+            3 => Event::OnFall,
+            4 => Event::OnValueReachVal,
+            5 => Event::OnValueEqVal,
+            6 => Event::OnValueLeaveVal,
+            7 => Event::OnValueDiffVal,
+            8 => Event::OnCrossHighLimit,
+            9 => Event::OnCrossLowLimit,
+            10 => Event::OnOverHighLimit,
+            11 => Event::OnUnderLowLimit,
+            12 => Event::OnValueReachRange,
+            13 => Event::OnValueInRange,
+            14 => Event::OnValueLeaveRange,
+            15 => Event::OnValueOutRange,
+            _ => Event::OnSet
+        }
+    }
+    
+    fn _cast_var_events(var_events: Vec<VarEvent>) -> Vec<VariableEvent> {
+
+        var_events.into_iter().map(|vevent| VariableEvent {
+            event: vevent.event as i32,
+            value: vevent.value,
+            low_limit: vevent.low_limit,
+            high_limit: vevent.high_limit
+        }).collect()
+
+    }
+
+}
